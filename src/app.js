dad494a0f963a6e160b0fad7e539a21420b1b250
woodajung <da2146@naver.com>
2022-07-19 21:35:16 +0900

    서버

--- src/app.js
+++ src/app.js
@@ -19,9 +19,10 @@ import { authenticateUser } from './utils/auth.js';
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 mongoose.connect(
-  'mongodb+srv://test:1234@cluster0-ypgh5.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://woodajung:1234@cluster0.l6phm.mongodb.net/Cluster0?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true
   },
 );
 mongoose.Promise = global.Promise;
